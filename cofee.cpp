b9845e7da3970b32e9dff03341512c51dc36a0ec
SNEHAL <96332827+codingstarsnehal@users.noreply.github.com>
2022-11-09 22:50:38 +0530

    Add files via upload
    Assignment 2.

---
+++ cofee.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int ch;
+    cout<<"ch=1 for cofee and ch=2 for tea"<<endl;
+    cout<<"enter choice";
+    cin>>ch;
+
+    if(ch=1)
+    {
+        cout<<"enjoy your cofee"<<endl;
+    }
+    else if(ch=2){
+        cout<<"enjoy your tea";
+        cout<<endl;
+    }
+    else{
+        cout<<"invalid choice";
+    }
+    return 0;
+}
