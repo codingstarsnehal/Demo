1db5fd620bfa17bf4c8ad297a05a9be39ddf490b
SNEHAL <96332827+codingstarsnehal@users.noreply.github.com>
2022-11-13 23:01:30 +0530

    Add files via upload
    implementing even odd problem using if else.

--- evenodd.cpp
+++ evenodd.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 int main()
 {
-    int n;
-    cout<<"enter the number"<<endl;
-    cin>>n;
-    if(n%2==0)
-    {
-        cout<<"the number is even"<<endl;
-    }
-    else{
-        cout<<"the number is odd"<<endl;
-    }
-    return 0;
+	int n;
+	cout<<"enter the number"<<endl;
+	cin>>n;
+	if(n%2==0)
+	{
+		cout<<"The number is even"<<endl;
+	}
+	else
+	{
+		cout<<"The number is odd"<<endl;
+	}
+	return 0;
 }
