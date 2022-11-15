faad9e45301ba142e9cc4b8d3c8cdab9b4f292e8
SNEHAL <96332827+codingstarsnehal@users.noreply.github.com>
2022-11-15 15:07:33 +0530

    Add files via upload
    Program tp implementing reverse pyramid.

---
+++ reversepyramid.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	for(int i=1;i<=5;i++)
+	{
+		
+		for(int k=1;k<i;k++)
+		{
+			cout<<" ";
+		}
+		for(int j=i;j<=(8-i);j++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
