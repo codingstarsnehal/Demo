3f2c2b2b8e07ff2d199f645355c69031ef109e64
SNEHAL <96332827+codingstarsnehal@users.noreply.github.com>
2022-11-18 15:22:03 +0530

    Add files via upload
    Linear search using functions and global declarations.

---
+++ searchglobal.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+int i,ele,a[]={2,5,3,9,1};
+void search()
+{
+	
+	int n=sizeof(a)/sizeof(int);
+	cout<<"Enter the element to be searched"<<endl;
+	cin>>ele;
+for(i=0;i<=4;i++)
+	{
+		if(a[i]==ele)
+		{
+		cout<<"The Element is found"<<endl;
+		cout<<"The Element is found at index"<<i;
+		break;
+		}
+	
+	}
+	if(i==n)
+	{
+	cout<<"The Element not found";
+	}
+}
+int main()
+{
+
+
+	search();
+	
+	return 0;
+}
+
+
+Linear search using functions
+
+#include<iostream>
+using namespace std;
+
+void search()
+{
+	int i,ele,a[]={2,5,3,9,1};
+	int n=sizeof(a)/sizeof(int);
+	cout<<"Enter the element to be searched"<<endl;
+	cin>>ele;
+for(i=0;i<=4;i++)
+	{
+		if(a[i]==ele)
+		{
+		cout<<"The Element is found"<<endl;
+		cout<<"The Element is found at index"<<i;
+		break;
+		}
+	
+	}
+	if(i==n)
+	{
+	cout<<"The Element not found";
+	}
+}
+int main()
+{
+
+
+	search();
+	
+	return 0;
+}
