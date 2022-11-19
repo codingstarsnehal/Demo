7c3ea651c4c6d65e05acae425cc187a059d01e22
SNEHAL <96332827+codingstarsnehal@users.noreply.github.com>
2022-11-19 21:00:54 +0530

    Add files via upload
    Implementing binary search using functions.

---
+++ binaryserch1.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+void search()
+{
+	int a[]={6,30,85,90,95,98,100};
+	int n=sizeof(a)/sizeof(int);
+	int s=0,e=n-1,val,res=-1;
+	while(s<=e)
+	{
+	int mid=(s+e)/2;
+		if(a[mid]==val)
+		{
+		res=mid;
+		cout<<"Element is found at"<<mid;
+		break;
+		}
+		else if(a[mid]>val)
+		{
+		e=mid-1;
+		}
+		else
+		{
+			s=mid+1;
+		}
+		
+	}
+	if(res==-1)
+	{
+	cout<<"Element is not found";
+	}	
+}
+
+int main()
+{
+	//int a[]={6,30,85,90,95,98,100};
+//	int n=sizeof(a)/sizeof(int);
+	//int s=0,e=n-1,val,res=-1;
+	int val,res=-1;
+	cout<<"Enter the value to be searched";
+	cin>>val;
+	search();
+	return 0;
+
+
+}
